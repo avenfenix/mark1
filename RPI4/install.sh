c3299bd009c804c01410525fad71b37fc942d800
Avenfenix <thedonjuan008p@gmail.com>
2023-09-18 19:06:50 +0100

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -4,10 +4,15 @@ USERNAME="$1"
 
 echo "Por favor verifique que este conectado el USB1"
 
-## Change hostname
+## CHANGE HOSTNAME
 rm /etc/hosts
 cp ./etc/* /etc/
 
+## HOME
+cd /home/$USER
+rm -r *
+
+## MONTAR USB1 AUTOMATICAMENTE
 UUID=$(blkid -s UUID -o value /dev/sda1)
 echo "UUID=$UUID /home/$USERNAME/USB1 ext4 user,errors=remount-ro,auto,exec,rw 0 0" | sudo tee -a /etc/fstab
 
