c92954449ecacafe96afcd43a672ef462eaedee9
Avenfenix <thedonjuan008p@gmail.com>
2023-09-18 19:01:53 +0100

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+USERNAME="$1"
+
+echo "Por favor verifique que este conectado el USB1"
+
 ## Change hostname
 rm /etc/hosts
 cp ./etc/* /etc/
 
-sh ./scripts/usb1.sh
+UUID=$(blkid -s UUID -o value /dev/sda1)
+echo "UUID=$UUID /home/$USERNAME/USB1 ext4 user,errors=remount-ro,auto,exec,rw 0 0" | sudo tee -a /etc/fstab
