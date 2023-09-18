9f295b152bbb6456c5b8a069832658d30b582778
Avenfenix <thedonjuan008p@gmail.com>
2023-09-18 21:33:58 +0100

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -27,7 +27,7 @@ apt-get install firefox-esr
 
 ### SUNSHINE
 wget https://github.com/LizardByte/Sunshine/releases/download/v0.20.0/sunshine-debian-bullseye-arm64.deb
-apt-get install -f sunshine-debian-bullseye-arm64.deb
+apt-get install -f ./sunshine-debian-bullseye-arm64.deb
 
 ### MOONLIGHT
 curl -1sLf 'https://dl.cloudsmith.io/public/moonlight-game-streaming/moonlight-qt/setup.deb.sh' | distro=raspbian codename=buster sudo -E bash
