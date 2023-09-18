52b1c75dcf5c240e9546fda22d93efaaa813ee7d
Avenfenix <thedonjuan008p@gmail.com>
2023-09-18 19:04:24 +0100

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -10,3 +10,12 @@ cp ./etc/* /etc/
 
 UUID=$(blkid -s UUID -o value /dev/sda1)
 echo "UUID=$UUID /home/$USERNAME/USB1 ext4 user,errors=remount-ro,auto,exec,rw 0 0" | sudo tee -a /etc/fstab
+
+## ACTUALIZAR SISTEMA
+apt-get update
+apt-get upgrade
+
+## SOFTWARE
+apt-get install firefox-esr                         
+apt-get install git                                  
+curl -fsSL https://code-server.dev/install.sh | sh
