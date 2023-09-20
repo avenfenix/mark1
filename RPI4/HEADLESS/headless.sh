7214cdd4f0ee61f16d275da147806a50b884c28d
Avenfenix <thedonjuan008p@gmail.com>
2023-09-20 04:55:24 -0300

    Update headless.sh

--- RPI4/HEADLESS/headless.sh
+++ RPI4/HEADLESS/headless.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-## IP ESTATICA USB0
-echo "interface usb0" >> /etc/dhcp.conf
-echo static ip_address=192.168.42.2/24 >> /etc/dhcpcd.conf
-
 ###  SUNSHINE
 wget https://github.com/LizardByte/Sunshine/releases/download/v0.20.0/sunshine-debian-bullseye-arm64.deb
 apt-get install -f ./sunshine-debian-bullseye-arm64.deb
