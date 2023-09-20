23782993494902572358edae8b4abcf8f2132bea
Avenfenix <thedonjuan008p@gmail.com>
2023-09-20 05:33:07 -0300

    Update install-service.sh

--- RPI4/HEADLESS/install-service.sh
+++ RPI4/HEADLESS/install-service.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo 'KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/85-sunshine.rules
 mkdir /home/$USER/.config/systemd/
 mkdir /home/$USER/.config/systemd/user/
 cp ./sunshine.service /home/$USER/.config/systemd/user/
