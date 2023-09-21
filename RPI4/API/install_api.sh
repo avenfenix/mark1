cb567e74567a12b26ae815ed6aa6f31409e485b4
Avenfenix <thedonjuan008p@gmail.com>
2023-09-21 00:17:36 -0300

    Update install_api.sh

--- RPI4/API/install_api.sh
+++ RPI4/API/install_api.sh
@@ -5,7 +5,11 @@
 ## codigo para instalar
 mkdir /home/$USER/bin/
 cp ./rpi-api /home/$USER/bin/
+cp ./rpiapi.service rpiapi.service.orig
+sed -i "s/user/$USER/g" rpiapi.service
 cp ./rpiapi.service /home/$USER/.config/systemd/user/rpiapi.service
+rm ./rpiapi.service
+mv ./rpiapi.service.orig ./rpiapi.service
 
 systemctl --user enable rpiapi.service
 systemctl --user start rpiapi.service
