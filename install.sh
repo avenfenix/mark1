336d63a2577efc6cd21dd7d4b83ad3683f159dc5
Avenfenix <thedonjuan008p@gmail.com>
2023-10-12 16:14:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
-
 cd ~
 # Actualizar
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install git tmux
+
+git clone https://github.com/avenfenix/mark1
 
 # Instalar Dotnet
 wget https://download.visualstudio.microsoft.com/download/pr/a2afb4da-478b-4ffa-aeeb-a37a313d6fc8/0897a8456d42f306519de9a15b1e14ef/dotnet-sdk-6.0.414-linux-arm64.tar.gz
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-6.0.414-linux-arm64.tar.gz -C $HOME/dotnet
+echo "export DOTNET_ROOT=$HOME/dotnet" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/dotnet" >> $HOME/.bashrc
+echo "alias openbullet2=\"tmux new -d -s openbullet2 'bash ~/mark1/openbullet2.sh'\"" >> ~/.bashrc
 source .bashrc
 
 # Instalar OpenBullet
