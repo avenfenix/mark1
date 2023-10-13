a1fcbbb3c462f6d853bb8a1949383e1be5b21948
Avenfenix <thedonjuan008p@gmail.com>
2023-10-13 20:24:37 -0300

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -8,8 +8,8 @@ wget https://github.com/Kron4ek/Wine-Builds/releases/download/8.0.1/wine-8.0.1-x
 tar xvf wine-8.0.1-amd64.tar.xz
 tar xvf wine-8.0.1-x86.tar.xz
 rm wine-8.0.1-amd64.tar.xz wine-8.0.1-x86.tar.xz
-mv wine-8.0.1-amd64 wine64
-mv wine-8.0.1-x86 wine
+sudo mv wine-8.0.1-amd64 wine64
+sudo mv wine-8.0.1-x86 wine
 
 echo 'export DISPLAY=:0
 export BOX86_PATH=~/wine/bin/
@@ -42,62 +42,4 @@ wine64 npp.8.5.2.Installer.x64.exe
 
 wine64 ~/.wine32/drive_c/Program\ Files/Notepad++/notepad++.exe
 
-cd ~/
-wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-mv winetricks /usr/local/bin/
 
-echo '#!/bin/bash 
-export BOX86_NOBANNER=1 WINE=wine WINEPREFIX=~/.wine32 WINESERVER=~/wine/bin/wineserver
-wine '"/usr/local/bin/winetricks "'"$@"' > /usr/local/bin/winetricks32
-chmod +x /usr/local/bin/winetricks32
-echo '#!/bin/bash 
-export BOX64_NOBANNER=1 WINE=wine64 WINEPREFIX=~/.wine64 WINESERVER=~/wine64/bin/wineserver
-wine64 '"/usr/local/bin/winetricks "'"$@"' > /usr/local/bin/winetricks64
-
-winetricks32 --gui
-winetricks64 --gui
-
-winetricks32 dotnet20sp2
-winetricks64 dotnet20sp2
-
-cd ~/Desktop
-echo '[Desktop Entry]
-Name=Wine32 Explorer
-Exec=bash -c "wine explorer"
-Icon=wine
-Type=Application' > ~/Desktop/Wine32_Explorer.desktop
-chmod +x ~/Desktop/Wine32_Explorer.desktop
-cp ~/Desktop/Wine32_Explorer.desktop /usr/share/applications/
-
-cd ~/Desktop
-echo '[Desktop Entry]
-Name=Wine64 Explorer
-Exec=bash -c "wine64 explorer"
-Icon=wine
-Type=Application' > ~/Desktop/Wine64_Explorer.desktop
-chmod +x ~/Desktop/Wine64_Explorer.desktop
-cp ~/Desktop/Wine64_Explorer.desktop /usr/share/applications/
-
-cd ~/Desktop
-echo '[Desktop Entry]
-Name=Winetricks32 Explorer
-Exec=bash -c "winetricks32 --gui"
-Icon=wine
-Type=Application' > ~/Desktop/Winetricks32_gui.desktop
-chmod +x ~/Desktop/Winetricks32_gui.desktop
-cp ~/Desktop/Winetricks32_gui.desktop /usr/share/applications/
-
-cd ~/Desktop
-echo '[Desktop Entry]
-Name=Winetricks64 Explorer
-Exec=bash -c "winetricks64 --gui"
-Icon=wine
-Type=Application' > ~/Desktop/Winetricks64_gui.desktop
-chmod +x ~/Desktop/Winetricks64_gui.desktop
-cp ~/Desktop/Winetricks64_gui.desktop /usr/share/applications/
-
-
-chmod +x /usr/local/bin/winetricks64
-
-sudo apt-get install -y zenity
