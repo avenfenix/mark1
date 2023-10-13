391573e122cf3286679a9d6c5f071cb71b6ab8a1
Avenfenix <thedonjuan008p@gmail.com>
2023-10-13 18:35:18 -0300

    Update box64.sh

--- box64.sh
+++ box64.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
-wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
-sudo apt update && sudo apt install box64-arm64 -y
-
+cd ~
+git clone https://github.com/ptitSeb/box64
+cd box64
+mkdir build; cd build; cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make -j2
+sudo make install
+sudo systemctl restart systemd-binfmt
